b95dda12124d6da817b1a14c293e357b97957f66
linda <lindaailekti2001@gmail.com>
2025-11-13 11:13:06 +0100

    ajout un initiale

---
+++ classe.java
@@ -0,0 +1,33 @@
+
+/**
+ * Décrivez votre classe classe ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class classe
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    /**
+     * Constructeur d'objets de classe classe
+     */
+    public classe()
+    {
+        // initialisation des variables d'instance
+        x = 0;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public int sampleMethod(int y)
+    {
+        // Insérez votre code ici
+        return x + y;
+    }
+}
