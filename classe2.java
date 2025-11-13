75dad9874fa1b149e715b8a1af4339db49edd9f0
linda <lindaailekti2001@gmail.com>
2025-11-13 12:53:37 +0100

    classe2

---
+++ classe2.java
@@ -0,0 +1,33 @@
+
+/**
+ * Décrivez votre classe classe2 ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class classe2
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    /**
+     * Constructeur d'objets de classe classe2
+     */
+    public classe2()
+    {
+        // initialisation des variables d'instance
+        x = 024;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public int sampleMethod(int y)
+    {
+        // Insérez votre code ici
+        return x + y;
+    }
+}
